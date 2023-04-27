6d2c512eabc56dec655ceb1e23f0104c12a92c85
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-04-28 00:30:39 +0530

    api for get single data

--- infinity/client/src/components/pages/student/StudentDetail.jsx
+++ infinity/client/src/components/pages/student/StudentDetail.jsx
@@ -1,16 +1,25 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 export const StudentDetail = () => {
-    // const [detail, setSetail] = ({})
+    const [detail, setDetail] = useState()
     const {id} = useParams()
-    console.log(id);
 
-   
+    const getSIngledata = ()=>{
+        axios.get(`http://localhost:8080/getSingledata/${id}/singlestudent`)
+        .then((res)=>{
+            setDetail(res.data.getsingle)
+            // console.log(res.data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
 
     useEffect(()=>{
-    },[])
+        getSIngledata()
+    },[]);
   return (
     <div>StudentDetail</div>
   )
