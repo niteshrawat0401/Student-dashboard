21c7692b92755d0aabbc1873dc092d5e7309aeff
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-05-19 01:12:54 +0530

    add readme

---
+++ infinity/README.md
@@ -0,0 +1,25 @@
+# Student
+
+This a small student platform for student.
+
+### Pages & Features 👇
+
+- `Home Page`: Navbar where you can see Dashboard and student.
+- `Sign-In`:  User authentication to check whether user email and password match with details existing in the database.
+- `Search`: User can search student present.
+- `Add student`: Here we can add data.
+- `Edit`: User can be edit the detail.
+- `Delete`: User can be delete the detail.
+- `Checkbox`: through checkbox user will show active inactive.
+- `Pagination`: There is pagination to change the page.
+- `Detail display`: User can see his whole detail in new page.
+
+
+## Tech Stack
+- React js
+- Javascript - (Local Storage)
+- Node js
+- Express js
+- Mongodb
+- Tailwind
+- Material UI
---
+++ infinity/client/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import {Route, Routes} from "react-router-dom"
+import {Signup} from "./components/pages/Signup"
+import {Login} from "./components/pages/Login"
+import { Home } from './components/Home/Home';
+import { Navbar } from './components/pages/Navbar';
+import { Student } from './components/pages/student/Student';
+import { Editstudent } from './components/pages/student/Editstudent';
+import { StudentDetail } from './components/pages/student/StudentDetail';
+import {Quicknotes} from './components/pages/Quicknotes/Quicknotes';
+import { Addquicknotes } from './components/pages/Quicknotes/Addquicknotes';
+import  ScssFile  from './components/pages/ScssFile';
+import { PrivateRoute } from './components/pages/PrivateRoute';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/student' element={
+        <PrivateRoute>
+        <Student/>
+        </PrivateRoute>
+        }/>
+        {/* <Route path='/' element={<PrivateRoute><Home/></PrivateRoute>}/> */}
+        <Route path='/edit/:id' element={<Editstudent/>}/>
+        <Route path='/detail/:id' element={<StudentDetail/>}/>
+        <Route path='/detail/:id' element={<StudentDetail/>}/>
+        <Route path='/quicknotes' element={<Quicknotes/>}/>
+        <Route path='/addquicknotes' element={<Addquicknotes/>}/>
+
+      </Routes>
+      {/* <ScssFile/> */}
+    </div>
+  );
+}
+
+export default App;
